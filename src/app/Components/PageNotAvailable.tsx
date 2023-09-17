6ca336826095eb6ed0ecc61b8fee0d39a8e47f59
Olumide Igbiloba <enocholumide@gmail.com>
2023-09-17 18:05:52 +0200

    Update font

--- src/app/Components/PageNotAvailable.tsx
+++ src/app/Components/PageNotAvailable.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageNotYetAvailable = () => {
     return (
         <div className="w-full h-full flex flex-col py-0">
-            <p className="text-4xl font-thin md:text-7xl mb-3">Page not yet available</p>
+            <p className="text-7xl font-thin mb-3">Page not yet available</p>
             <p className="md:text-3xl">Please check again later, thank you!</p>
         </div>
     )
